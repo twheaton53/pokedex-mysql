ef37a60f4a18f9f50950c226c3c2d18e05083632
Travis Wheaton <traviswheaton53@gmail.com>
2021-04-10 14:01:46 -0700

    Rendered react elements on page

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded( { extended: true } ));
-app.use(express.static(path.join(__dirname + '../client/dist')));
+app.use(express.static(path.join(__dirname + '/../client/dist')));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
