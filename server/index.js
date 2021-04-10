5c2d95d788d600f8d9af73ff22105bf1eecaadeb
Travis Wheaton <traviswheaton53@gmail.com>
2021-04-10 13:55:16 -0700

    Installed dependences & built express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-// TODO: Create an express server
+const express = require('express');
+const path = require('path');
+const db = require('./db');
+const port = 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded( { extended: true } ));
+app.use(express.static(path.join(__dirname + '../client/dist')));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
